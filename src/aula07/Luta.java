a7c806e966f12eb81c60027939c6c50fed764214
natashareis <40079286+natashareis@users.noreply.github.com>
2019-03-19 00:03:30 -0300

    Initial commit

---
+++ src/aula07/Luta.java
@@ -0,0 +1,12 @@
+package aula07;
+/*
+public interface Luta {
+	public abstract void apresentar();
+	public abstract void Status();
+	public abstract void ganharLuta();
+	public abstract void perderLuta();
+	public abstract void empatarLuta();
+	
+
+}
+*/
